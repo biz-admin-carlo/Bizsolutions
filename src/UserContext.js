f09f83cc530c32db4808ce30a4fa3ba8d2a1e1a7
carloicorcuera <carloicorcuera@gmail.com>
2024-03-15 00:53:48 +0800

    cleaned package.json and updated Payroll Pricing

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-// Initializes a react context
 const UserContext = React.createContext();
 
-// Initializes a context provider
-// Gives us ability to provide a specific context through component
 
 export const UserProvider = UserContext.Provider;
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Updated import statement
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import 'leaflet/dist/leaflet.css';
 
-
-// Initialize AOS
 AOS.init();
 
 const container = document.getElementById('root');
-const root = createRoot(container); // Updated usage of createRoot
+const root = createRoot(container); 
 root.render(
     <App />
 );
