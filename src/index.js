73c0d37ea167d5c3436b5e9d190999bbcb9ca132
carloicorcuera <carloicorcuera@gmail.com>
2024-03-16 11:33:26 +0800

    added Chat Support

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'aos/dist/aos.css';
 import 'leaflet/dist/leaflet.css';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('YOUR_TRACKING_ID'); // Replace 'YOUR_TRACKING_ID' with your actual GA tracking ID
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 AOS.init();
