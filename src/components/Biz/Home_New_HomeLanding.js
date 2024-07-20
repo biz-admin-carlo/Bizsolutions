dda394cf0b0ead389526f29cde8127ec64d98869
carloicorcuera <carloicorcuera@gmail.com>
2024-07-20 13:14:00 +0800

    added changes for New_HomeLanding_Responsiveness

--- src/components/Biz/Home_New_HomeLanding.js
+++ src/components/Biz/Home_New_HomeLanding.js
@@ -20,10 +20,10 @@ const HomeLanding = () => {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCurrentTagline((currentTagline + 1) % taglines.length); // Cycle through taglines
-        }, 2000); // Change tagline every 2 seconds
+            setCurrentTagline((currentTagline + 1) % taglines.length); 
+        }, 2000);
 
-        return () => clearInterval(intervalId); // Clean up the interval on component unmount
+        return () => clearInterval(intervalId);
     }, [currentTagline]);
 
     return (
@@ -43,20 +43,20 @@ const HomeLanding = () => {
                 </div>
             </div>
 
-            {/* <div className="section-container-video my-3">
+            <div className="section-container-video my-3">
                 <video autoPlay loop muted playsInline className="video-background">
                     <source src={stockVideo} type="video/mp4" />
                     Your browser does not support HTML5 video.
                 </video>
                 <div className="flex-row">
                     <div className="flex-col-video">
-                        <h1 className='title-brilliance px-5 ms-5'>
-                                Behind the brilliance
+                        <h1 className='title-brilliance pe-5'>
+                                Discover the Secret to Success
                         </h1>
-                        <h1 className='title-secondary px-5 ms-5'>{taglines[currentTagline]}</h1>
+                        <h1 className='title-secondary'>{taglines[currentTagline]}</h1>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </>
     );
 };
