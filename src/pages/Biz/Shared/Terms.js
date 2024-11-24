e9270ea8e752c81ee6cb9ba739494b7a6499fd75
carloicorcuera <carloicorcuera@gmail.com>
2024-11-24 14:34:25 +0800

    added v.1.3.17

--- src/pages/Biz/Shared/Terms.js
+++ src/pages/Biz/Shared/Terms.js
@@ -30,76 +30,92 @@ export default function Terms() {
                           </div>
 
                           <div className='children-card'>
-                          <p className="mb-2">Scope and Nature of Servicese</p>
-                          <Card.Subtitle className="mb-2"><span className='text-intro'>Website Development:</span> We offer comprehensive web design and development services. This encompasses custom programming, e-commerce solutions, integration of content management systems, mobile application development, and ongoing website maintenan</Card.Subtitle>
+                          <p className="mb-2">Scope and Nature of Services</p>
+                          <Card.Subtitle className="mb-2"><span className='text-intro'>Website Development:</span> We offer comprehensive web design and development services. This encompasses custom programming, e-commerce solutions, integration of content management systems, mobile application development, and ongoing website maintenance.</Card.Subtitle>
                           
-                          <Card.Subtitle className="mb-2"><span className='text-intro'>Business Listing Services:</span>  We offer digital platforms tailored for business listings to enhance your online presence. Our services include optimizing listings for search engines (SEO), managing customer reviews, and providing detailed analytics reports.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2"><span className='text-intro'>Business Listing Services:</span> We offer digital platforms tailored for business listings to enhance your online presence. Our services include optimizing listings for search engines (SEO), managing customer reviews, and providing detailed analytics reports.
+                          </Card.Subtitle>
 
-                          <Card.Subtitle className="mb-2"><span className='text-intro'>Bookkeeping Services: </span>  Our financial record management includes comprehensive services such as general ledger maintenance, managing accounts payable and receivable, payroll processing, and generating financial reports, ensuring your financial records are accurate and up-to-date.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2"><span className='text-intro'>Bookkeeping Services: </span> Our financial record management includes comprehensive services such as general ledger maintenance, managing accounts payable and receivable, payroll processing, and generating financial reports, ensuring your financial records are accurate and up-to-date.
+                          </Card.Subtitle>
 
-                          <Card.Subtitle className="mb-2"><span className='text-intro'>Outsourcing Support Services: </span>  We provide a range of administrative, technical, and specialized support services for various business processes that our clients choose to outsource, ensuring efficiency and quality in outsourced operations.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2"><span className='text-intro'>Outsourcing Support Services: </span> We provide a range of administrative, technical, and specialized support services for various business processes that our clients choose to outsource, ensuring efficiency and quality in outsourced operations.
+                          </Card.Subtitle>
 
-                          <Card.Subtitle className="mb-2"><span className='text-intro'>IT Support Services: </span>  Our IT support encompasses a wide range of services, including system setup, network configuration, implementing cybersecurity solutions, managing data backup and recovery, and offering ongoing IT consultations to keep your technology infrastructure robust and secure.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2"><span className='text-intro'>IT Support Services: </span> Our IT support encompasses a wide range of services, including system setup, network configuration, implementing cybersecurity solutions, managing data backup and recovery, and offering ongoing IT consultations to keep your technology infrastructure robust and secure.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Registration, User Accounts, and Responsibilities</p>
-                          <Card.Subtitle className="mb-2">To access specific services, users are required to register and create an account. By doing so, users commit to providing current, complete, and accurate information for all purchases and account activities. It is the responsibility of the user to promptly update their account and other relevant information, including email addresses and credit card numbers, to facilitate seamless transactions and enable efficient communication when necessary.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">To access specific services, users are required to register and create an account. By doing so, users commit to providing current, complete, and accurate information for all purchases and account activities. It is the responsibility of the user to promptly update their account and other relevant information, including email addresses and credit card numbers, to facilitate seamless transactions and enable efficient communication when necessary.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Privacy Policy and Data Protection</p>
-                          <Card.Subtitle className="mb-2">Respect for user privacy is a cornerstone of our operations. Our Privacy Policy outlines our practices concerning the collection, use, and sharing of personal data. By utilizing our services, you acknowledge and consent to the processing of your data in accordance with these practices. Additionally, you affirm that all data provided by you is accurate and up-to-date.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">Respect for user privacy is a cornerstone of our operations. Our Privacy Policy outlines our practices concerning the collection, use, and sharing of personal data. By utilizing our services, you acknowledge and consent to the processing of your data in accordance with these practices. Additionally, you affirm that all data provided by you is accurate and up-to-date.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">User Conduct and Compliance with Laws</p>
-                          <Card.Subtitle className="mb-2">Users are strictly prohibited from using our services for any unlawful purposes or engaging in activities that violate any local, state, national, or international laws or regulations. This prohibition includes, but is not limited to, adherence to data privacy laws, intellectual property laws, and export control laws. It is the responsibility of the user to ensure their activities conform to all applicable legal standards.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">Users are strictly prohibited from using our services for any unlawful purposes or engaging in activities that violate any local, state, national, or international laws or regulations. This prohibition includes, but is not limited to, adherence to data privacy laws, intellectual property laws, and export control laws. It is the responsibility of the user to ensure their activities conform to all applicable legal standards.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Intellectual Property Rights and Content Ownership</p>
-                          <Card.Subtitle className="mb-2">All content provided on or through the services, such as text, graphics, logos, images, as well as the compilation thereof, and any software used on the service, is the property of BizSolutions LLC or its suppliers and protected by copyright and other intellectual property laws.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">All content provided on or through the services, such as text, graphics, logos, images, as well as the compilation thereof, and any software used on the service, is the property of BizSolutions LLC or its suppliers and protected by copyright and other intellectual property laws.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Service Modifications, Interruptions, and Termination</p>
-                          <Card.Subtitle className="mb-2">We reserve the right to modify, suspend, or discontinue, temporarily or permanently, the service (or any part thereof) with or without notice. We shall not be liable to you or to any third party for any modification, price change, suspension, or discontinuance of the service. Accounts that are found to be in violation of these TOS may be terminated or suspended.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">We reserve the right to modify, suspend, or discontinue, temporarily or permanently, the service (or any part thereof) with or without notice. We shall not be liable to you or to any third party for any modification, price change, suspension, or discontinuance of the service. Accounts that are found to be in violation of these Terms of Service (TOS) may be terminated or suspended.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Warranties and Limitation of Liability</p>
-                          <Card.Subtitle className="mb-2">We do not guarantee, represent, or warrant that your use of our service will be uninterrupted, timely, secure, or error-free. We do not warrant that the results that may be obtained from the use of the service will be accurate or reliable.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">We do not guarantee, represent, or warrant that your use of our service will be uninterrupted, timely, secure, or error-free. We do not warrant that the results that may be obtained from the use of the service will be accurate or reliable.
+                          </Card.Subtitle>
                           </div>
                           
                           <div className='children-card'>
                           <p className="mb-2">Indemnification</p>
-                          <Card.Subtitle className="mb-2">You agree to indemnify, defend, and hold harmless BizSolutions LLC and our partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns, and employees from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these TOS or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">You agree to indemnify, defend, and hold harmless BizSolutions LLC and our partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns, and employees from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service (TOS) or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Fees, Payments, and Billing</p>
-                          <Card.Subtitle className="mb-2">Certain services may require payment of fees. Users agree to pay all fees and charges on time, and BizSolutions LLC reserves the right to suspend or terminate access to services for any accounts with overdue balances.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">Certain services may require payment of fees. Users agree to pay all fees and charges on time, and BizSolutions LLC reserves the right to suspend or terminate access to services for any accounts with overdue balances.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Cancellation, Refund, and Return Policy</p>
-                          <Card.Subtitle className="mb-2">Users may cancel services under certain conditions outlined in our cancellation policy. We will issue refunds in accordance with our refund policy, which is detailed on our website.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">Users may cancel services under certain conditions outlined in our cancellation policy. We will issue refunds in accordance with our refund policy, which is detailed on our website.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Governing Law and Dispute Resolution</p>
-                          <Card.Subtitle className="mb-2">These TOS and any separate agreements whereby we provide you services shall be governed by and construed in accordance with the laws of [Your State/Country]. Any disputes or claims arising out of or in connection with these TOS will be subject to the exclusive jurisdiction of the state and federal courts located in [Your Jurisdiction].</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">These Terms of Service (TOS) and any separate agreements whereby we provide you services shall be governed by and construed in accordance with the laws of California. Any disputes or claims arising out of or in connection with these Terms of Service (TOS) will be subject to the exclusive jurisdiction of the state and federal courts located in California.
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Entire Agreement and Severability</p>
-                          <Card.Subtitle className="mb-2">These TOS and any policies or operating rules posted by us on this site or in respect to the service constitute the entire agreement and understanding between you and us. If any provision of these TOS is determined to be unlawful, void, or unenforceable, such provision shall nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed severed from these TOS.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">These Terms of Service (TOS) and any policies or operating rules posted by us on this site or in respect to the service constitute the entire agreement and understanding between you and us. If any provision of these Terms of Service (TOS) is determined to be unlawful, void, or unenforceable, such provision shall nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed severed from these Terms of Service (TOS).
+                          </Card.Subtitle>
                           </div>
 
                           <div className='children-card'>
                           <p className="mb-2">Contact Information and Notices</p>
-                          <Card.Subtitle className="mb-2">Questions about the Terms of Service should be sent to us at [Contact Information]. All notices to users will be posted on our website or sent via email.</Card.Subtitle>
+                          <Card.Subtitle className="mb-2">Questions about the Terms of Service should be sent to us at +1 833-249-7418. All notices to users will be posted on our website or sent via email
+                          </Card.Subtitle>
                           </div>
 
                       </Card.Body>
