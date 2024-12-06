99eea8523ff1f7aea92a7ff1d22e58bb870e6164
carloicorcuera <carloicorcuera@gmail.com>
2024-12-06 22:43:15 +0800

    added v1.3.19.1 - updated register bizness

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'aos/dist/aos.css';
 import 'leaflet/dist/leaflet.css';
 import ReactGA from 'react-ga';
 
-const APP_VERSION = '1.3.19';
+const APP_VERSION = '1.3.19.1';
 
 function checklistUser() {
     const storedLocalVersion = localStorage.getItem('appVersion');
